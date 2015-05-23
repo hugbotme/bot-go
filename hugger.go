dc5adb799ad9cbab51fad155c284a7b15e75bd7e
jvanthoor <jan.vanthoor@trivago.com>
2015-05-23 22:57:38 +0200

    attach git repo reader to file processer

--- hugger.go
+++ hugger.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	//"gopkg.in/libgit2/git2go.v22"
 	"github.com/hugbotme/bot-go/parser"
+	"bytes"
 )
 
 
@@ -15,14 +16,26 @@ func processHug(url *Hug) {
 
 	parser := parser.NewParser("mre", repoName)
 
+	// jvt: @todo this could all be streamed through memory as a byte stream
 	lines, err := parser.GetFileContents("Readme.md")
 
+	var buffer bytes.Buffer
 	if err != nil {
 		log.Printf("Error during clone: %v\n", err)
 	} else {
 		for i, line := range lines {
 			fmt.Println(i, line)
+			buffer.WriteString(line)
 		}
+
+		processor, err := newSpellCheckFileProcessor()
+		if err != nil {
+			fmt.Errorf("could not get speller: %s", err.Error())
+			return
+		}
+
+		content := processor.processContent([]byte(buffer.String()))
+		fmt.Println("corrected content: " + string(content))
 	}
 
 	branchname := "bugfix"
@@ -32,20 +45,4 @@ func processHug(url *Hug) {
 
 	parser.CommitFile(branch, branchname, "Readme.md", lines, "Fixing some typos")
 	parser.PullRequest("A friendly pull request")
-
-	files := []string{
-		"test string one",
-		"another awesome test string",
-	}
-
-	processor, err := newSpellCheckFileProcessor()
-	if err != nil {
-		fmt.Errorf("could not get speller: %s", err.Error())
-		return
-	}
-
-	for _, file := range files {
-		content := processor.processContent([]byte(file))
-		fmt.Println("corrected content: " + string(content))
-	}
 }
