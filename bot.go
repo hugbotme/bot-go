4210945dcd6e02088061e6235bc313d0b44ec990
jvanthoor <jan.vanthoor@trivago.com>
2015-05-23 13:47:57 +0200

    main loop with interupt

--- bot.go
+++ bot.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	//"github.com/hugbotme/go-aspell"
+	"os"
+	"os/signal"
 )
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("here we go #hahaha")
+
+	// capture ctrl+c and stop execution
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	for {
+		// jvt: run as long as no interrupt is sent
+		go func() {
+			for sig := range c {
+				fmt.Printf("captured %v, notifying everyone...\n", sig)
+
+				fmt.Println("exiting...")
+				os.Exit(1)
+			}
+		}()
+
+		// jvt: do things...
+	}
 }
